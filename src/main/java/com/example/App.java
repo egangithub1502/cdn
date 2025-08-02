f908be82830a4fa485393916007ec1e61cea7089
jegan <2017107071b@gmail.com>
2025-08-02 22:16:41 +0530

    ✅ Working Java file

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Build successful!");
+        System.out.println("Missing semicolon")
     }
 }
 
