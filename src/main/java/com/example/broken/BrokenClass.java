9be6a9fee148035dfc9468ff8b5a366eb27559b6
jegan <2017107071b@gmail.com>
2025-08-09 10:17:33 +0530

    committest

--- src/main/java/com/example/broken/BrokenClass.java
+++ src/main/java/com/example/broken/BrokenClass.java
@@ -2,7 +2,6 @@ package com.example.broken;
 
 public class BrokenClass {
     public void test() {
-        // Missing semicolon will cause compilation error
-        System.out.println("This will fail")
+        System.out.println("This will work");
     }
 }
