b330f372172881430f0f8f1630780b118a0500a8
jegan <2017107071b@gmail.com>
2025-08-09 10:30:51 +0530

    new

--- src/main/java/com/example/broken/BrokenClass.java
+++ src/main/java/com/example/broken/BrokenClass.java
@@ -2,6 +2,7 @@ package com.example.broken;
 
 public class BrokenClass {
     public void test() {
-        System.out.println("This will work");
+        // Missing semicolon to cause compilation failure
+        System.out.println("This will fail")
     }
 }
